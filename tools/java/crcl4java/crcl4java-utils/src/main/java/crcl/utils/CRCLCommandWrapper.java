e30871f011c545ad0b73b6a78837ee20795b4ec8
Will Shackleford <william.shackleford@nist.gov>
2021-12-15 11:14:20 -0500

    test github actions

--- tools/java/crcl4java/crcl4java-utils/src/main/java/crcl/utils/CRCLCommandWrapper.java
+++ tools/java/crcl4java/crcl4java-utils/src/main/java/crcl/utils/CRCLCommandWrapper.java
@@ -29,7 +29,6 @@ import crcl.base.MiddleCommandType;
 import java.util.concurrent.ConcurrentLinkedDeque;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-
 /*
  * 
  * NOTE: Comments beginning with {@literal @} or {@literal >>>} are used by Checker Framework Comments
